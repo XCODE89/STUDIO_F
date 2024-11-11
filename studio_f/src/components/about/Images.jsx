8ade209b2472b5675aa5a058430a81358df8e374
XCODE89 <maph0989@gmail.com>
2024-11-11 11:32:34 -0300

    * cases grid

--- studio_f/src/components/about/Images.jsx
+++ studio_f/src/components/about/Images.jsx
@@ -1,6 +1,6 @@
 const Images = () =>{
     return(
-        <div className="w-full">
+        <div className="w-full px-6">
             <div className="grid grid-cols-2 gap-8">
 
             <img src='https://images.unsplash.com/photo-1546913199-55e06682967e?ixlib=rb-1.2.1&auto=format&fit=crop&crop=focalpoint&fp-x=.735&fp-y=.55&w=1184&h=1376&q=80'
